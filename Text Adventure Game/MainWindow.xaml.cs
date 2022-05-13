9e88dd62143e0af4d23b07b738969c3414a2195b
zonedetec@gmail.com <zonedetec@gmail.com>
2022-05-13 20:19:48 +0200

    Ajoutez des fichiers projet.

---
+++ Text Adventure Game/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Text_Adventure_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_Settings_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+                ShowSettings();
+        }
+
+        private void ShowSettings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
